09a0d7c864903e196a636471c34973dceab37ebf
Aziz Arfaoui <aziz99arfaoui@gmail.com>
2025-11-21 20:10:52 +0100

    Update project files

--- WhyNotTry/ContentView.swift
+++ WhyNotTry/ContentView.swift
@@ -14,13 +14,15 @@ struct ContentView: View {
     var activities = ["Archery", "Baseball", "Basketball", "Bowling", "Boxing", "Cricket", "Curling", "Fencing", "Golf", "Hiking", "Lacrosse", "Rugby", "Squash"]
 
     @State var selected = "Baseball"
-    
+    @State private var id = 1
+
     
     var body: some View {
         VStack {
             Text("Why not try…")
                 .font(.largeTitle.bold())
 
+            
             VStack {
                 Circle()
                     .fill(colors.randomElement() ?? .blue)
@@ -34,11 +36,16 @@ struct ContentView: View {
                 Text("\(selected)!")
                     .font(.title)
             }
+            .transition(.slide)
+            .id(id)
         }
+        
         Spacer()
+        
         Button("Try again") {
-            withAnimation {
+            withAnimation(.easeInOut(duration: 1)) {
                     selected = activities.randomElement() ?? "Archery"
+                    id += 1
                 }
         }
         .buttonStyle(.borderedProminent)
