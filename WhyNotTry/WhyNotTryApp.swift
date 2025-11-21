34e94b54e51408937287273e2a18ec3aa10bbc74
Aziz Arfaoui <aziz99arfaoui@gmail.com>
2025-11-21 18:53:55 +0100

    Initial Commit

---
+++ WhyNotTry/WhyNotTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhyNotTryApp.swift
+//  WhyNotTry
+//
+//  Created by Aziz Arfaoui on 21/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct WhyNotTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
